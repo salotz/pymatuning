211620222d7f924ae9757f57f06c03e96e7af004
Samuel Lotz <samuel.lotz@salotz.info>
2018-12-04 13:46:27 -0500

    adding initial files

---
+++ pymatuning/cli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+
+import click
+
+@click.group()
+def cli():
+    pass
+
+if __name__ == "__main__":
+
+    cli()
+
+
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymatuning',
+    version='0.1',
+    author="Samuel D. Lotz",
+    author_email="samuel.lotz@salotz.info",
+    description="Small python library and CLI for generating todos from python modules",
+    py_modules=['pymatuning'],
+    url="https://github.com/salotz/pymatuning",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts' : [
+            "pymatuning = pymatuning.cli:cli"]}
+
+)
